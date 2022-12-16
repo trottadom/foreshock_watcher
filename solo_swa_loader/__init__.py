fd9715dba778dd6cbed4d0be220a9de67bd8f2f7
trottadom <d.trotta@imperial.ac.uk>
2022-12-16 16:04:54 +0000

    Developing now

--- solo_swa_loader/__init__.py
+++ solo_swa_loader/__init__.py
@@ -66,3 +66,49 @@ def swa_load_grnd_mom(startdate, enddate, path=None):
     print("Scanned...SCANNED")
 
     return df_solo_swa
+
+
+def swa_load_Eflux(startdate, enddate, path=None):
+    """
+    Load SolO/SWA L2 ground moments
+
+    Load-in data for Solar Orbiter/SWA sensor ground moments. Supports level 2
+    provided by ESA's Solar Orbiter Archive. Optionally downloads missing
+    data directly. Returns data as Pandas dataframe.
+
+    Parameters
+    ----------
+    startdate, enddate : {datetime, str, or int}
+        Datetime object (e.g., dt.date(2021,12,31) or dt.datetime(2021,4,15)),
+        "standard" datetime string (e.g., "2021/04/15") or integer of the form
+        yyyymmdd with empty positions filled with zeros, e.g. '20210415'
+        (enddate must always be later than startdate)
+    path : {str}, optional
+        Local path for storing downloaded data, by default None
+
+    Returns
+    -------
+    Pandas dataframe
+    """
+
+    if isinstance(startdate, int):
+        startdate = _date2str(startdate)
+    if isinstance(enddate, int):
+        enddate = _date2str(enddate)
+    
+    print("Scanning...scanned")
+
+    instrument = a.Instrument('SWA')
+    time = a.Time(startdate, enddate)
+    level = a.Level(2)
+    product = a.soar.Product('SWA-PAS-GRND-MOM')
+    
+    result = Fido.search(instrument & time & level & product)
+    files = Fido.fetch(result,path=path)
+    
+    solo_swa = TimeSeries(files, concatenate=True)
+    df_solo_swa = solo_swa.to_dataframe()
+
+    print("Scanned...SCANNED")
+
+    return df_solo_swa
